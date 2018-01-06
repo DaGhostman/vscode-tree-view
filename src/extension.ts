8e07ac9113fa1fd59cd84da6a4f80042354d8205
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-01-06 19:07:24 +0200

    Make available providers configurable (#7)
    * Allow some providers to be disabled via configuration
    
    Since the same provider is used for ts and js and ts is a superset of
    js, it makes sense the primary identifier to be `javascript`
    
    * replace `any` with stricter hinting
    
    * updated changelog

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,8 @@
 "use strict";
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from "vscode";
 import { Provider } from "./provider";
-import { JsonProvider, PhpProvider, TypescriptProvider } from "./providers";
-import { IBaseProvider } from "./providers/base";
-import { RuleProvider } from "./providers/openhab/rule";
+import { IBaseProvider, JsonProvider, PhpProvider, RuleProvider, TypescriptProvider } from "./providers";
 
 function goToDefinition(range: vscode.Range) {
     const editor: vscode.TextEditor = vscode.window.activeTextEditor;
@@ -17,35 +14,36 @@ function goToDefinition(range: vscode.Range) {
         // Swap the focus to the editor
         vscode.window.showTextDocument(editor.document, editor.viewColumn, false);
 }
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
-    const provider = new Provider([
-        new PhpProvider(),
-        new TypescriptProvider(),
-        new JsonProvider(),
-        new RuleProvider(),
-    ] as Array<IBaseProvider<any>>);
+    const providers: Array<IBaseProvider<string | vscode.TreeItem>> = [];
+    const config = vscode.workspace.getConfiguration("treeview");
+
+    const allowedProviders: string[] = config.has("allowedProviders") ?
+        config.get("allowedProviders") : [];
+
+    if (allowedProviders.length === 0 || allowedProviders.indexOf("php") !== -1) {
+        providers.push(new PhpProvider());
+    }
+
+    if (allowedProviders.length === 0 || allowedProviders.indexOf("javascript") !== -1) {
+        providers.push(new TypescriptProvider());
+    }
+
+    if (allowedProviders.length === 0 || allowedProviders.indexOf("json") !== -1) {
+        providers.push(new JsonProvider());
+    }
+
+    if (allowedProviders.length === 0 || allowedProviders.indexOf("openhab") !== -1) {
+        providers.push(new RuleProvider());
+    }
+
+    const provider = new Provider(providers as Array<IBaseProvider<string | vscode.TreeItem>>);
 
     vscode.window.registerTreeDataProvider("tree-outline", provider);
     vscode.commands.registerCommand("extension.treeview.goto", (range: vscode.Range) => goToDefinition(range));
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    // let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-    //     // The code you place here will be executed every time your command is executed
-
-    //     // Display a message box to the user
-    //     vscode.window.showInformationMessage('Hello World!');
-    // });
-
-    // context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
     return undefined;
 }
